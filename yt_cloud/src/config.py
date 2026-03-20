60607ba3cc1e3d63756607ab667b8b2024fdf2ad
Rishabh <rishabh27232525@gmail.com>
2026-03-20 12:19:58 +0530

    Add configuration file for project settings

---
+++ yt_cloud/src/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+OUTPUT_DIR = BASE_DIR.parent / "output"
+
+ACTIVE_LANGUAGES = ["en"]
+
+# Dummy placeholders (baad me change kar sakte)
+CHANNEL_NAME = "DarkMindVault"
